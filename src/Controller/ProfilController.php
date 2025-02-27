48f647ca17a83ee96e8f9f50c46f5c54385c76de
Quidé <quideclement@gmail.com>
2025-02-27 16:34:52 +0100

    commit

--- src/Controller/ProfilController.php
+++ src/Controller/ProfilController.php
@@ -6,13 +6,15 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class ProfilController extends AbstractController
+class ProfilController extends AbstractController
 {
     #[Route('/profil', name: 'app_profil')]
     public function index(): Response
     {
+        $info = ['lastname' => 'Loper', 'firstname' => 'Dave', 'email' => 'daveloper@code.dom', 'birthdate' => '01/01/1970'];
+
         return $this->render('profil/index.html.twig', [
-            'controller_name' => 'ProfilController',
+            'informations' => $info
         ]);
     }
 }
